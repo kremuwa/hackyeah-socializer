143c50ec0740c34d501effaef8bb5356e6d4fe96
Michał <kremuwa@gmail.com>
2022-11-19 23:43:00 +0100

    Added display of animal image when game is started

--- components/elements/Meeting.js
+++ components/elements/Meeting.js
@@ -1,5 +1,29 @@
-export const Meeting = ( props = {}) => {
-    const {gameParams = {}} = props;
-    const {colors, emoji} = gameParams;
-    return <p>Meeting</p>
-}
+import styled from "styled-components";
+import Twemoji from "react-twemoji";
+
+const EmojiWrapper = styled.div`
+  text-align: center;
+  background-color: ${(props) => props.backgroundColor};
+  & img {
+    width: 80%;
+  }
+  height: 100vh
+`;
+
+export const Meeting = (props) => {
+  const { gameParams = {} } = props;
+  const { color, emoji } = gameParams;
+
+  return (
+    <EmojiWrapper backgroundColor={color}>
+      <Twemoji
+        options={{
+          folder: "svg",
+          ext: ".svg",
+        }}
+      >
+        {emoji}
+      </Twemoji>
+    </EmojiWrapper>
+  );
+};
