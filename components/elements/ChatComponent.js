04d55146a21f8a4e5fa3ac89797420102a91f857
Jarosław <j.myjak@optimumpareto.com>
2022-11-20 02:09:29 +0100

    chat css

--- components/elements/ChatComponent.js
+++ components/elements/ChatComponent.js
@@ -2,28 +2,28 @@ import React, { useEffect, useState, useRef } from "react";
 import firebase from "@/firebase-client";
 import { useCollection } from 'react-firebase-hooks/firestore';
 import EmojiPicker, { Emoji } from "emoji-picker-react";
-
+import {useRouter} from "next/router";
 
 const ChatComponent = () => {
-	const chatId = 1;
-	const userId = 2;
+	const router = useRouter()
+    const {query = {}} = router;
+    const {id, userid} = query;
 
 	const [value, loading, error] = useCollection(
-		firebase.firestore().collection('chat'),
+		firebase.firestore().collection(`chat${id}`),
 	);
 
 	const [messages, setMessages] = useState([]);
 	const chatBottom = useRef(null);
 
-	const createMessage = (text, chatId, userId) => {
-		firebase.firestore().collection('chat').doc().set({
+	const createMessage = (text) => {
+		firebase.firestore().collection(`chat${id}`).doc().set({
 			created: Date.now(),
-			user: userId,
+			user: userid,
 			message: text
+		}).then(() => {
+			chatBottom.current?.scrollIntoView({ behavior: "smooth" })
 		})
-			.then(() => {
-				chatBottom.current?.scrollIntoView({ behavior: "smooth" })
-			})
 
 	};
 
@@ -41,10 +41,10 @@ const ChatComponent = () => {
 	return <>
 		<div className="chatWindow">
 			{error && <strong>Error: {JSON.stringify(error)}</strong>}
-			{loading && <span>Collection: Loading...</span>}
+			{loading && <span>Loading...</span>}
 			<>
 				{messages?.map(doc =>
-					<div key={doc.created} className={`message ${doc.user === userId ? "sent" : "received"}`}>
+					<div key={doc.created} className={`message ${doc.user === userid ? "sent" : "received"}`}>
 						<Emoji unified={doc.message} size={100} emojiStyle="twitter" />
 					</div>
 				)}
@@ -55,8 +55,9 @@ const ChatComponent = () => {
 			theme="dark"
 			emojiStyle="twitter"
 			skinTonesDisabled
+			suggestedEmojisMode="recent"
 			width={"100%"}
-			onEmojiClick={emoji => createMessage(emoji.unified, chatId, userId)} />
+			onEmojiClick={emoji => createMessage(emoji.unified)} />
 	</>;
 }
 
